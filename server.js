ca629c3911a6779ec3137b92ddc3b92d015f648f
Evgeniy Melnichenko <e.melnichenko@mil.gov.ua>
2021-02-23 21:02:25 +0200

    first build v3

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/CountTime'));
 
-app.use(express.static(__dirname + '/dist/CountTime'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/CountTime/'}),
+);
 
-app.get('/', function (req, res){
-    res.sendFile(path.join(__dirname + '/dist/CountTime'));
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
